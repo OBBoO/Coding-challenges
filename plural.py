79a3a4173ca44c1f761d033b2e9cf8d4f48e484c
benbella othmane <othmane.benbella@grenoble-em.com>
2020-11-12 21:15:30 +0100

    Send

---
+++ plural.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[50]:
+
+
+def pluralize(lst):
+    words=[]
+    for word in lst:
+        words.append([word,1])
+    d = {}
+    for element in words:
+        
+        if element[0] not in d:
+        
+            d[element[0]] = []
+        
+        d[element[0]].append(element[1])    
+    s={}
+    for i,j in d.items():
+        s[i]=sum(j)
+    final = set()
+    
+    for m,y in  s.items():
+        if y>1:
+            final.add(m+str("s"))
+        else:
+            final.add(m)
+    
+
+    return final
+pluralize(["othmane","othmane","raw","hey","lol"])
+
+
+# In[32]:
+
+
+d={}
+d["ZDZ"]=[5,4]
+for value in d:
+    print(d)
+
+
+# In[ ]:
+
+
+
+
